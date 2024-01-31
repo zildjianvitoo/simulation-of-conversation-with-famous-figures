151085301f005d3bd7d8e1a4fd278b3f607263a3
ZildjianVito <zildjianvito07@gmail.com>
2024-01-31 18:55:56 +0700

    feat: create folder structure

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-3xl text-green-300">kocak</div>;
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="text-3xl text-green-300">kocak</div>;
-}
